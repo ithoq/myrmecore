44c7da019ae939461b0e2734e6fa9305503f5ae4
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2010-12-31 16:53:36 +0000

    - start.php Added license header
    - welcome.php
    Added license header

--- trunk/www/application/controllers/welcome.php
+++ trunk/www/application/controllers/welcome.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * MyrmeCore
+ *
+ * A Web-based Wireless Sensor Network Management System
+ *
+ * @package		MyrmeCore
+ * @author		Juan F. Duque <jfelipe@grupodyd.com>
+ * @copyright	Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+ * @license		http://www.myrmecore.com/license/
+ * @link		http://www.myrmecore.com
+ * @since		Version 0.1
+ * @filesource
+ */
 
 class Welcome extends Controller {
 
