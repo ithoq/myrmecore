cec2927a71962ebb40ab883cb6f1077c927fe747
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2011-01-24 01:09:58 +0000

    - desktop.php PHP Controller that will load the desktop view
    - desktop_view.php
    PHP View that creates the desktop HTML
    
    - myrmecore.js
    JS file that creates the ExtJS Desktop layout

--- www/application/controllers/start.php
+++ www/application/controllers/start.php
@@ -4,26 +4,26 @@
  *
  * A Web-based Wireless Sensor Network Management System
  *
- * @package		MyrmeCore
- * @author		Juan F. Duque <jfelipe@grupodyd.com>
- * @copyright	Copyright (c) 2011, Dinamica y Desarrollo Ltda.
- * @license		http://www.myrmecore.com/license/
- * @link		http://www.myrmecore.com
- * @since		Version 0.1
+ * @package        MyrmeCore
+ * @author        Juan F. Duque <jfelipe@grupodyd.com>
+ * @copyright    Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+ * @license        http://www.myrmecore.com/license/
+ * @link        http://www.myrmecore.com
+ * @since        Version 0.1
  * @filesource
  */
 
 class Start extends Controller {
 
-	function Start()
-	{
-		parent::Controller();	
-	}
-	
-	function index()
-	{
-		$this->load->view('start_message');
-	}
+    function Start()
+    {
+        parent::Controller();    
+    }
+    
+    function index()
+    {
+        $this->load->view('start_message');
+    }
 }
 
 /* End of file start.php */
