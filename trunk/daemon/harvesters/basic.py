706da0861d9e50b9df613b812d9af819a4e9af0a
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2011-01-31 19:19:31 +0000

    - daemon Here we will store all system daemons
    - daemon/harvesters
    Here we will store the scripts that harvest data
    
    - basic.py
    Basic harvester, right now it's just a placeholder with license and a Hello World script

---
+++ trunk/daemon/harvesters/basic.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/python
+
+# MyrmeCore
+#
+# A Web-based Wireless Sensor Network Management System
+# 
+# @package		MyrmeCore
+# @author			Andres M. Eusse <aeusse@grupodyd.com>
+# @copyright		Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+# @license		http://www.myrmecore.com/license/
+# @link				http://www.myrmecore.com
+# @since			Version 0.1
+# @filesource
+
+print "Hello World!";
