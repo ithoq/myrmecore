87de134332ac32f6ba2e083135f459e901d9defc
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2011-02-01 06:16:21 +0000

    - basic.py Some tests with the Python ConfigParser module
    - basic.ini
    Default configuration file, to be parsed by basic.py

--- trunk/daemon/harvesters/basic.py
+++ trunk/daemon/harvesters/basic.py
@@ -4,12 +4,18 @@
 #
 # A Web-based Wireless Sensor Network Management System
 # 
-# @package		MyrmeCore
-# @author			Andres M. Eusse <aeusse@grupodyd.com>
+# @package			MyrmeCore
+# @authors			Juan F. Duque <jfelipe@grupodyd.com>, Andres M. Eusse <aeusse@grupodyd.com>
 # @copyright		Copyright (c) 2011, Dinamica y Desarrollo Ltda.
-# @license		http://www.myrmecore.com/license/
+# @license			http://www.myrmecore.com/license/
 # @link				http://www.myrmecore.com
 # @since			Version 0.1
 # @filesource
 
-print "Hello World!";
+import ConfigParser
+
+# Read basic.ini configuration file
+Config = ConfigParser.ConfigParser()
+Config.read("basic.ini")
+
+
