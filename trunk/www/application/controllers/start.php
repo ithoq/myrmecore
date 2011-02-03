3acdad47a29b1ee7a3e9f80831ed48a9b7109669
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2011-02-03 04:54:50 +0000

    - basic.py Added description
    - start.php
    Set start_view as the start controller default view
    
    - start.js & start.css
    Main components of Start widget (contents are from a non-related project)
    
    - client.py & server.py
    Placeholders for a more advanced harvester type

--- trunk/www/application/controllers/start.php
+++ trunk/www/application/controllers/start.php
@@ -1,15 +1,15 @@
-<?php
+﻿<?php
 /**
  * MyrmeCore
  *
  * A Web-based Wireless Sensor Network Management System
  *
- * @package        MyrmeCore
- * @author        Juan F. Duque <jfelipe@grupodyd.com>
- * @copyright    Copyright (c) 2011, Dinamica y Desarrollo Ltda.
- * @license        http://www.myrmecore.com/license/
- * @link        http://www.myrmecore.com
- * @since        Version 0.1
+ * @package        	MyrmeCore
+ * @author        	Juan F. Duque <jfelipe@grupodyd.com>
+ * @copyright    	Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+ * @license        	http://www.myrmecore.com/license/
+ * @link        	http://www.myrmecore.com
+ * @since        	Version 0.1
  * @filesource
  */
 
@@ -22,7 +22,7 @@ class Start extends Controller {
     
     function index()
     {
-        $this->load->view('start_message');
+        $this->load->view('start_view');
     }
 }
 
---
+++ trunk/www/application/views/start_view.php
@@ -0,0 +1,37 @@
+﻿<!--
+MyrmeCore
+
+A Web-based Wireless Sensor Network Management System
+
+@package        	MyrmeCore
+@author        		Juan F. Duque <jfelipe@grupodyd.com>
+@copyright    		Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+@license        	http://www.myrmecore.com/license/
+@link        		http://www.myrmecore.com
+@since        		Version 0.1
+@filesource
+-->
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title><?php echo $title; ?></title>
+<base href="<?php echo base_url(); ?>" />
+
+<!-- STYLESHEETS -->
+<link rel="stylesheet" type="text/css" href="../../js/resources/css/ext-all.css" />
+<link rel="stylesheet" type="text/css" href="widgets/css/start.css" />
+
+<!-- LIBRARIES -->
+<script type="text/javascript" src="../../js/adapter/ext/ext-base.js"></script>
+<script type="text/javascript" src="../../js/ext-all.js"></script>
+
+<!-- WIDGETS -->
+<script type="text/javascript" src="widgets/js/start.js"></script>
+
+</head>
+<body>
+<div id="loading-mask"></div>
+<div id="loading"></div>
+</body>
+</html>
+
