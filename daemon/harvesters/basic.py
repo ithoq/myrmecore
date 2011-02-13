11188a9d8badd7a83732c4b21229b14935b04d9d
jfduque <jfduque@a137c21a-179f-ec1b-d89e-3322d1cbef6b>
2011-02-13 07:49:34 +0000

    - basic.py Some work at reading basic.ini configuration file and writing logs about it

--- daemon/harvesters/basic.py
+++ daemon/harvesters/basic.py
@@ -1,15 +1,15 @@
-﻿#!/usr/bin/python
+#!/usr/bin/python
 
 # MyrmeCore
 #
 # A Web-based Wireless Sensor Network Management System
 # 
-# @package			MyrmeCore
-# @authors			Juan F. Duque <jfelipe@grupodyd.com>, Andres M. Eusse <aeusse@grupodyd.com>
-# @copyright		Copyright (c) 2011, Dinamica y Desarrollo Ltda.
-# @license			http://www.myrmecore.com/license/
-# @link				http://www.myrmecore.com
-# @since			Version 0.1
+# @package                      MyrmeCore
+# @authors                      Juan F. Duque <jfelipe@grupodyd.com>, Andres M. Eusse <aeusse@grupodyd.com>
+# @copyright            	Copyright (c) 2011, Dinamica y Desarrollo Ltda.
+# @license                      http://www.myrmecore.com/license/
+# @link                         http://www.myrmecore.com
+# @since                        Version 0.1
 # @filesource
 
 # basic.py - Basic Data Harvester
@@ -19,9 +19,44 @@
 
 import ConfigParser
 import logging
+from sqlalchemy import *
 
 # Read basic.ini configuration file
 Config = ConfigParser.ConfigParser()
 Config.read("basic.ini")
 
+# Create logger
+logLevel = "logging." + Config.get("main","Level")
+logFile = Config.get("main","Logfile")
+basic_logger = logging.getLogger("basic_harvester")
+basic_logger.setlevel = Config.get("main","Level")
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',filename=logFile)
+
+logging.debug('Harvester Status: STARTED')
+logging.info('Harvester Status: STARTED')
+
+# Connect to Database
+
+dbDriver = Config.get("database","Driver")
+print dbDriver
+
+if dbDriver == "mysqli":
+  logging.debug('MySQL driver selected')
+  logging.info('MySQL driver selected')
+  dbhost = Config.get("database","Hostname")
+  dbuser = Config.get("database","Username")
+  dbpass = Config.get("database","Password")
+  dbname = Config.get("database","Database")
+  db = create_engine('mysql://scott:tiger@localhost/demodb')
+elif dbDriver == "postgres":
+  logging.debug('PostgreSQL driver selected')
+  logging.info('PostgreSQL driver selected')
+  dbhost = Config.get("database","Hostname")
+  dbuser = Config.get("database","Username")
+  dbpass = Config.get("database","Password")
+  dbname = Config.get("database","Database")
+  db = create_engine('postgres://scott:tiger@localhost/demodb')
+else:
+  logging.debug('NO database driver has been selected')
+  logging.info('NO database driver has been selected')
 
